27337cd487577684c896e1a3622818be2b12e0fb
Bram Holtackers <bholtackers@outlook.com>
2019-09-10 09:15:30 +0200

    Changes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('COM3', {
+const port = new SerialPort('COM5', {
     baudRate: 9600
 });
 const parser = port.pipe(new Readline({
